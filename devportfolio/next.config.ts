5a2bfed7f4a3640f1dcf1cdeac4393d00c46ab72
rohanvenkatesha <rohanvenkatesha@gmail.com>
2025-01-05 18:52:03 -0500

    update

--- devportfolio/next.config.ts
+++ devportfolio/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: '/devportfolio', // Correct base path for your repository      // Ensure URLs have a trailing slash
+  output: 'export',          // Use static export instead of next export
 };
 
 export default nextConfig;
