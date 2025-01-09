8b323f42c05228e3b414e288076724ee03c24555
rohanvenkatesha <rohanvenkatesha@gmail.com>
2025-01-09 18:48:44 -0500

    update skills

--- devportfolio/components/main/Skills.tsx
+++ devportfolio/components/main/Skills.tsx
@@ -1,5 +1,7 @@
+import { Frontend_skill } from '@/constants';
 import { transform } from 'next/dist/build/swc/generated-native'
 import React from 'react'
+import SkillDataProvider from '../sub/SkillDataProvider';
 
 const Skills = () => {
   return (
@@ -9,45 +11,15 @@ const Skills = () => {
     className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'
     style={{transform: 'scale(0.9)'}}>
         <div className='flex flex-row justify-around flex-wrap mt-4 gap-10 items-center'>
-            
+            {Frontend_skill.map((image, index) => (
+                <SkillDataProvider
+                key={index}
+                src={image.Image}
+                width={image.width}
+                height={image.height}
+                index={index}/>
+            ))}
         </div>
-        {/* <h1 className='text-4xl font-bold text-white'>Skills</h1>
-        <div className='flex flex-row gap-10 mt-10'>
-            <div className='flex flex-col gap-5'>
-                <h1 className='text-2xl font-bold text-white'>Languages</h1>
-                <div className='flex flex-row gap-5'>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-xl font-bold text-white'>Python</h1>
-                        <h1 className='text-xl font-bold text-white'>JavaScript</h1>
-                        <h1 className='text-xl font-bold text-white'>TypeScript</h1>
-                        <h1 className='text-xl font-bold text-white'>Java</h1>
-                    </div>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-xl font-bold text-white'>Go</h1>
-                        <h1 className='text-xl font-bold text-white'>C++</h1>
-                        <h1 className='text-xl font-bold text-white'>C#</h1>
-                        <h1 className='text-xl font-bold text-white'>SQL</h1>
-                    </div>
-                </div>
-            </div>
-            <div className='flex flex-col gap-5'>
-                <h1 className='text-2xl font-bold text-white'>Frameworks</h1>
-                <div className='flex flex-row gap-5'>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-xl font-bold text-white'>Django</h1>
-                        <h1 className='text-xl font-bold text-white'>Flask</h1>
-                        <h1 className='text-xl font-bold text-white'>Express.js</h1>
-                        <h1 className='text-xl font-bold text-white'>React.js</h1>
-                    </div>
-                    <div className='flex flex-col gap-2'>
-                        <h1 className='text-xl font-bold text-white'>Next.js</h1>
-                        <h1 className='text-xl font-bold text-white'>Angular</h1>
-                        <h1 className='text-xl font-bold text-white'>Spring Boot</h1>
-                        <h1 className='text-xl font-bold text-white'>ASP.NET</h1>
-                    </div>
-                </div>
-            </div>
-        </div> */}
     </section>
   );
 };
--- devportfolio/components/sub/SkillDataProvider.tsx
+++ devportfolio/components/sub/SkillDataProvider.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
+import Image from 'next/image';
 
 
 interface Props {
@@ -11,8 +12,34 @@ interface Props {
     index: number;
     }
 const SkillDataProvider = ({src,width,height,index}: Props) => {
+    const {ref, inView} = useInView({
+        triggerOnce: true,
+        threshold: 0.5
+    })
+
+    const imageVariants = {
+        hidden: {opacity: 0},
+        visible: {opacity: 1}}
+
+    const animationDelay = 0.3
+
   return (
-    <div>SkillDataProvider</div>
+    <motion.div
+    ref={ref}
+    initial='hidden'
+    variants={imageVariants}
+    animate={inView ? 'visible' : 'hidden'}
+    custom={index}
+    transition={{duration: 0.5, delay: index * animationDelay}}
+    >
+        <Image
+        src={src}
+        width={width}
+        height={height}
+        alt='skill image'
+        />
+
+    </motion.div>
   )
 }
 
