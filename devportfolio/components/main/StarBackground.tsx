a76055761e8bbb1d24bde078e758b96339cdb49c
rohanvenkatesha <rohanvenkatesha@gmail.com>
2025-01-09 22:54:34 -0500

    Update StarBackground.tsx

--- devportfolio/components/main/StarBackground.tsx
+++ devportfolio/components/main/StarBackground.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, { useState, useRef, Suspense } from "react";
+import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial } from "@react-three/drei";
-//@ts-ignore
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+// @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: any) => {
@@ -13,23 +13,40 @@ const StarBackground = (props: any) => {
   );
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
-  });
+    ref.current.rotation.x -= delta/10;
+    ref.current.rotation.y -= delta/15;
+  })
+
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
-        <PointMaterial
-          transparent
-          color="#fff"
-          size={0.002}
-          sizeAttenuation={true}
-          depthWrite={false}
-        />
-      </Points>
+        <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled
+        {...props}
+        >
+            <PointMaterial
+                transparent
+                color="$fff"
+                size={0.002}
+                sizeAttenuation={true}
+                dethWrite={false}
+            />
+        </Points>
     </group>
-  );
+  )
 };
 
-export default StarBackground;
+const StarsCanvas = () => (
+    <div className="w-full h-auto fixed inset-0 z-[20]">
+        <Canvas camera={{position: [0, 0, 1]}}>
+        <Suspense fallback={null}>
+            <StarBackground />
+        </Suspense>
+        </Canvas>
+    </div>
+)
+
+export default StarsCanvas;
