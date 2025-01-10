d0f6ed669ebdf350f38724d7f181817149ddea2a
rohanvenkatesha <rohanvenkatesha@gmail.com>
2025-01-09 22:52:47 -0500

    test

--- devportfolio/app/layout.tsx
+++ devportfolio/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist} from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/main/Navbar";
+// import StarCanvas from "@/components/main/StarBackground";
+
+import dynamic from 'next/dynamic';
+const StarCanvas = dynamic(() => import('@/components/main/StarBackground'));
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +34,7 @@ export default function RootLayout({
         {/* // className={`${geistSans.className} overflow-y-scroll overflow-x-hidden`}
         // style={{ background: 'linear-gradient(to bottom,rgb(0, 0, 0),rgba(0, 0, 3, 0.82))' }}>
         // className={`${geistSans.className} bg-gradient-to-b from-[#000000] to-[#1a1a1a] overflow-y-scroll overflow-x-hidden`}> */}
-
+        <StarCanvas/>
         <Navbar/>
         {children}
       </body>
