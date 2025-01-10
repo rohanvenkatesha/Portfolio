25ce2c9d1d5ffd3b5cbe4c6cc4b63474f6b61cdb
rohanvenkatesha <rohanvenkatesha@gmail.com>
2025-01-10 02:24:35 -0500

    set

--- devportfolio/app/layout.tsx
+++ devportfolio/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/main/Navbar";
-import StarCanvas from "@/components/main/StarBackground";
+// import StarCanvas from "@/components/main/StarBackground";
 
 // import dynamic from 'next/dynamic';
 // const StarCanvas = dynamic(() => import('@/components/main/StarBackground'), { ssr: false });
@@ -31,7 +31,7 @@ export default function RootLayout({
         {/* // className={`${geistSans.className} overflow-y-scroll overflow-x-hidden`}
         // style={{ background: 'linear-gradient(to bottom,rgb(0, 0, 0),rgba(0, 0, 3, 0.82))' }}>
         // className={`${geistSans.className} bg-gradient-to-b from-[#000000] to-[#1a1a1a] overflow-y-scroll overflow-x-hidden`}> */}
-        <StarCanvas/>
+        {/* <StarCanvas/> */}
         <Navbar/>
         {children}
       </body>
